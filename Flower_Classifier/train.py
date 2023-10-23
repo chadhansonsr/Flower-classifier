47ddaf12a2196177586c90d6f86309dcec06ad63
Chad Hanson <chad.hanson@stellantis.com>
2023-10-23 12:11:07 -0400

    added print statements for sanity checking

--- Flower_Classifier/train.py
+++ Flower_Classifier/train.py
@@ -18,7 +18,7 @@ from functions import load_data
 
 parser = argparse.ArgumentParser(description="Train a new neural network")
 
-parser.add_argument("--data_dir", type=str, default="C:/Users/t9349ch/Desktop/SWX/Udacity/Python Projects/Flower classifier/flowers", help="path to data directory")
+parser.add_argument("--data_dir", type=str, default=r"C:/Users/t9349ch/Desktop/SWX/Udacity/Python Projects/Flower classifier/flowers", help="path to data directory")
 parser.add_argument("--save_dir", type=str, default="checkpoint.pth", help="path to save model")
 parser.add_argument("--arch", type=str, default="vgg16", help="architecture (default: vgg16)")
 parser.add_argument("--learning_rate", type=float, default=0.001, help="learning rate (default: 0.001)")
@@ -41,3 +41,8 @@ training_data, validation_data, testing_data, trainloader, validloader, testload
 print(args.data_dir)
 print(args.save_dir)
 print(args.arch)
+print(args.learning_rate)
+print(args.hidden_units)
+print(args.epochs)
+print(args.GPU)
+

8b28b40bd4c3627dee75de8dacb9871e44f2243e
Chad Hanson <chad.hanson@stellantis.com>
2023-10-23 12:59:29 -0400

    fixed data_dir location

--- Flower_Classifier/train.py
+++ Flower_Classifier/train.py
@@ -18,7 +18,7 @@ from functions import load_data
 
 parser = argparse.ArgumentParser(description="Train a new neural network")
 
-parser.add_argument("--data_dir", type=str, default=r"C:/Users/t9349ch/Desktop/SWX/Udacity/Python Projects/Flower classifier/flowers", help="path to data directory")
+parser.add_argument("--data_dir", type=str, default=r"/Users/t9349ch/Desktop/SWX/Udacity/Python Projects/Flower classifier/flowers", help="path to data directory")
 parser.add_argument("--save_dir", type=str, default="checkpoint.pth", help="path to save model")
 parser.add_argument("--arch", type=str, default="vgg16", help="architecture (default: vgg16)")
 parser.add_argument("--learning_rate", type=float, default=0.001, help="learning rate (default: 0.001)")

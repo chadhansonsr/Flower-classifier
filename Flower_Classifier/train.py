16a41df218dfba27f53464e98e088345f765da46
Chad Hanson <chad.hanson@stellantis.com>
2023-10-23 13:39:44 -0400

    updated data dictionary arg

--- Flower_Classifier/train.py
+++ Flower_Classifier/train.py
@@ -1,9 +1,6 @@
+# Local working directory "/Users/t9349ch/Desktop/SWX/Udacity/Python Projects/Flower classifier/flowers"
+
 import argparse
-
-# %matplotlib inline
-# %config InlineBackend.figure_format = 'retina'
-# import matplotlib.pyplot as plt
-
 import torch
 from torch import nn
 from torch import optim
@@ -13,13 +10,12 @@ import json
 from torch.utils.data import DataLoader
 from PIL import Image
 import numpy as np
-import seaborn as sb
 from functions import load_data
 
 parser = argparse.ArgumentParser(description="Train a new neural network")
 
-parser.add_argument("--data_dir", type=str, default=r"/Users/t9349ch/Desktop/SWX/Udacity/Python Projects/Flower classifier/flowers", help="path to data directory")
-parser.add_argument("--save_dir", type=str, default="checkpoint.pth", help="path to save model")
+parser.add_argument("data_directory", action="store", help="path to data directory")
+parser.add_argument("--save_directory", type=str, default="checkpoint.pth", help="path to save model")
 parser.add_argument("--arch", type=str, default="vgg16", help="architecture (default: vgg16)")
 parser.add_argument("--learning_rate", type=float, default=0.001, help="learning rate (default: 0.001)")
 parser.add_argument("--hidden_units", type=int, default=256, help="hidden units (default: 256)")
@@ -28,8 +24,8 @@ parser.add_argument("--GPU", action="store_true", help="use GPU for training")
 
 args = parser.parse_args()
 
-data_dir = args.data_dir
-save_dir = args.save_dir
+data_dir = args.data_directory
+save_dir = args.save_directory
 arch = args.arch
 learning_rate = args.learning_rate
 hidden_units = args.hidden_units
@@ -38,11 +34,5 @@ gpu = args.GPU
 
 training_data, validation_data, testing_data, trainloader, validloader, testloader = load_data(data_dir)
 
-print(args.data_dir)
-print(args.save_dir)
+print(args.data_directory)
 print(args.arch)
-print(args.learning_rate)
-print(args.hidden_units)
-print(args.epochs)
-print(args.GPU)
-
